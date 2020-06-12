67e46e31a160ee96203207aaa98ac159b11e7bb4
Sergey Kuznetsov <kuznetsov.su.mobile@gmail.com>
2020-06-12 12:32:55 +0400

    third commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 "use strict";
-console.log("hello bro");
 const numberOfFilms = parseInt(prompt('How much movies do you already wached?'));
 if (isNaN(numberOfFilms)) {
-    console.log('you intered not a number');
-}
+    console.log('intered not numbers');
+}   else if (numberOfFilms <= 10) {
+        console.log('why so small?');
+    }
+else if ((numberOfFilms > 10) && (numberOfFilms <= 30)) {
+        console.log('you are good wiever');
+    }
+else {console.log('very good expert in films!');
+    }
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -12,11 +18,16 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('last wached movie name'),
-    b = prompt('please rate the movie'),
-    c = prompt('last wached movie name'),
-    d = prompt('please rate the movie');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
+Array = ['last wached movie name', 'please rate the movie'];
+    for (let i = 0; i < 2; i++) {
+        let z = prompt(Array[0]);
+        let x = prompt(Array[1]);
+        if ((z != '') && (z != null) && (z.length < 50) && (x != '') && (x != null)) {
+            personalMovieDB.movies[z] = x;
+            console.log('done');
+        } else {
+            i--;
+            console.log('error');
+        }
+    }
 console.log(personalMovieDB);
